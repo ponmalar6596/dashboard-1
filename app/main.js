6dd255bb946275b50d48f8647aa3b91a6a8c55e6
ponmalar6596 <ponmalar6596@gmail.com>
2020-11-22 23:15:10 -0800

    new fixes

---
+++ app/main.js
@@ -0,0 +1,8 @@
+function myfunction(){
+    var y = document.getElementById("sidebar-wrapper");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
