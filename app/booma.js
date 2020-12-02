9da20d8f1c6a16a313c8caf742071fe711a76bed
Booma4827 <mypersonal4827@gmail.com>
2020-12-02 10:52:34 +0530

    change file names

--- app/booma.js
+++ app/booma.js
@@ -1,26 +1,8 @@
-
-  function myFunctionnn() {
+function myFunctionnn() {
     var y = document.getElementById("ds");
     if (y.style.display === "none") {
-      y.style.display = "block";
-    } 
-    else {
-      y.style.display = "none";
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
     }
-  }
-//   $(document).ready(function(){
-//     $("#apply").click(function(){
-//         var input=$("#fname").val();
-//         var clr=$("#colors").val();
-//         var sizess=$("#sizes").val();
-//         var bgclr=$("#bgcolor").val();
-//         $('#design').append('<p id="result"></p>');
-//         $('#result').text(input);
-//         $('#result').css('font-size',sizess+'px');
-//         $('#result').css('color',clr);
-//         $('#result').css('background-color',bgclr);
-
-//     })
-// });
-
-
+}
