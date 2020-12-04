b54e87870000e06ecfd38d48822f4525f4d19561
ponmalar6596 <ponmalar6596@gmail.com>
2020-12-04 04:33:43 -0800

    change form html

--- app/js/patients.js
+++ app/js/patients.js
@@ -13,17 +13,14 @@
           $('#patientslist').html('');
           result.forEach(element => {
             let tr = `<tr class="t-row" >
-            <td>`+element._id +`</td>
+           
             <td>`+element.name +`</td>
             <td>`+element.phoneNo +`</td>
             <td>`+element.pincode +`</td>
             <td>`+element.streetName +`</td>
             <td>`+element.areaName +`</td>
             <td>`+element.tesedOn +`</td>
-            <td>`+element.resultsArrivedOn +`</td>
-            <td>`+element.quarantineStartedOn +`</td>
-            <td>`+element.curedOn +`</td>
-            <td>`+element.underWentHospitalization +`</td>
+           
             </tr>`
             $(`#patientslist`).append(tr);
             
