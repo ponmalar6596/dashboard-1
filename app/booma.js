36c8554d909a2a455457f0cb97bebc4a12025cb5
ponmalar6596 <ponmalar6596@gmail.com>
2020-12-04 07:15:49 -0800

    delete unwanted files

--- app/booma.js
+++
@@ -1,8 +0,0 @@
-function myFunctionnn() {
-    var y = document.getElementById("ds");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    } else {
-        y.style.display = "none";
-    }
-}
