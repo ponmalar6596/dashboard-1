b29c0ba094ca7c5a8eb53ab7dcc20a1d8f39f832
ponmalar6596 <ponmalar6596@gmail.com>
2020-12-07 02:01:51 -0800

    add patient

--- app/js/patients.js
+++ app/js/patients.js
@@ -20,7 +20,7 @@
             <td>`+element.streetName +`</td>
             <td>`+element.areaName +`</td>
             <td>`+element.tesedOn +`</td>
-           
+           <td><a href="patientsform.html" class="btn btn-primary">Edit</a></td>
             </tr>`
             $(`#patientslist`).append(tr);
             
@@ -29,3 +29,30 @@
       }
     });
   })
+
+
+  $('#submit').on('click',function (e) {
+    e.preventDefault();
+    var data = {
+       name:$('#name').val(),
+       phoneNo:$('#phoneNo').val(),
+       pincode: $('#pincode').val(),
+       streetName: $('#streetName').val(),
+       areaName: $('#areaName').val(),
+       tesedOn: $('#tesedOn').val()
+    }
+    console.log(data);
+  
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url: "http://localhost:3000/patients",
+        data : JSON.stringify(data),
+        dataType : 'json',
+        success: function (result) {
+            console.log(result+"done");
+        }
+    });
+    $("#patientform")[0].reset();
+});
+
