6abc307f36e870582a6ecd061d8f1466362fd6a9
ponmalar6596 <ponmalar6596@gmail.com>
2021-01-11 09:39:53 +0530

    dashboard

--- app/js/patients.js
+++ app/js/patients.js
@@ -2,10 +2,13 @@
 //    getpatients();
 //   });
 //   function getpatients(){
+
+//patients
   $(()=>{
     // var myusername = $("#username").val();
     $.ajax({
       url: "http://localhost:3000/patients",
+     
     //   data: myusername,
       // cache: false,
       success: function(result){
@@ -20,7 +23,10 @@
             <td>`+element.streetName +`</td>
             <td>`+element.areaName +`</td>
             <td>`+element.tesedOn +`</td>
-           <td><a href="patientsform.html" class="btn btn-primary">Edit</a></td>
+            <td>`+element.status +`</td>
+
+           <td><a class="btn btn-primary" id="edit" href="patientsform.html" onclick="location.href=this.href+'?patient_id=${element._id}';return false;">Edit</a></td>
+
             </tr>`
             $(`#patientslist`).append(tr);
             
@@ -39,7 +45,8 @@
        pincode: $('#pincode').val(),
        streetName: $('#streetName').val(),
        areaName: $('#areaName').val(),
-       tesedOn: $('#tesedOn').val()
+       tesedOn: $('#tesedOn').val(),
+       status: $('#status').val(),
     }
     console.log(data);
   
@@ -56,3 +63,69 @@
     $("#patientform")[0].reset();
 });
 
+  
+
+
+
+
+
+  function getPatientById(patientId){
+  $.ajax({
+      url: "http://localhost:3000/patients/findOne?patientId="+patientId,
+    //   data: myusername,
+      // cache: false,
+      success: function(result){
+     console.log("find one patient data ",result);
+     if(result.name){
+     $('#name').val(result.name);
+     }
+     if(result.phoneNo){
+     $('#phoneNo').val(result.phoneNo);
+     }
+     if(result.pincode){
+      $('#pincode').val(result.pincode);
+     }
+     if(result.streetName){
+      $('#streetName').val(result.streetName);
+     }
+     if(result.areaName){
+      $('#areaName').val(result.areaName);
+     }
+     if(result.tesedOn){
+      $('#tesedOn').val(result.tesedOn);
+     }
+     if(result.status){
+      $('#status').val(result.status);
+     }
+      }
+    })
+  }
+
+
+
+
+
+  //login
+
+
+
+  $('#login').on('click',function (e) {
+    e.preventDefault();
+    var data = {
+      UserName:$('#feFirstName').val(),
+      Password:$('#feEmailAddress').val()
+    }
+    console.log(data);
+  
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url:"https://orientation.boodskap.io/api/domain/login",
+        data : JSON.stringify(data),
+        dataType : 'json',
+        success: function (result) {
+            console.log(result+"done");
+        }
+    });
+    $("#form")[0].reset();
+});
